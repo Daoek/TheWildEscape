7f0c8d63dfe1fb6c2b4d96cfd7175a2832f60437
Loek van soest <Loek van soest@MSI>
2019-06-22 23:26:42 +0200

    added part of the worldborder mechanics

--- src/teamdjg/wildescape/main/Main.java
+++ src/teamdjg/wildescape/main/Main.java
@@ -1,26 +1,61 @@
 package teamdjg.wildescape.main;
 
+import org.bukkit.World;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin implements Listener	
+import teamdjg.wildescape.worldborder.WorldborderMechanics;
+
+public class Main extends JavaPlugin implements Listener 
 {
 	public String pluginPrefix = "[DJG TWE] ";
 	
-	//plugin enabled
+	public WorldborderMechanics _WorldborderMechanics;	
+	public World WorldBorderWorld;	
+	public long WorldBorderSpeed;	
+	public boolean WorldBorderIsRunning;
+	
+	public int WorldBorderMax;
+	public int WorldBorderMin;
+	public int WorldBorderCurrent;
+	public int WordBorderDistance;
+
+	
 	@Override
 	public void onEnable() 
 	{		
+		//set up references
+		new Eventhandler(this);
+		_WorldborderMechanics = new WorldborderMechanics(this);
+	
+		
 		System.out.println(pluginPrefix + "PLUGIN ENABLED!");
 		
-		//give the Eventhandler the instance
-		new Eventhandler(this);
 	}
 	
-	//plugin disabled
 	@Override
 	public void onDisable() 
 	{		
 		System.out.println(pluginPrefix + "PLUGIN DISABLED!");
 	}
+	
+	//Time checker for midnight
+		public void scheduleTimer(Main plugin, World world) {
+		    plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable() {
+		        public void run() {
+		            long time = world.getTime();
+		            
+		            if(time == 17800 && WorldBorderIsRunning)
+		            {
+		            	// print a warning to all players that border will be moving
+		            }
+		            
+		            if (time == 18000 && WorldBorderIsRunning) 
+		            {
+		            	// (print text and make border smaller) if enabled.
+		            }
+		            
+		        }
+		    }, 1, 1);
+		}
 }
--- src/teamdjg/wildescape/worldborder/WorldborderMechanics.java
+++ src/teamdjg/wildescape/worldborder/WorldborderMechanics.java
@@ -1,7 +1,5 @@
 package teamdjg.wildescape.worldborder;
 
-import org.bukkit.entity.Player;
-
 import teamdjg.wildescape.main.Main;
 
 public class WorldborderMechanics 
@@ -13,9 +11,48 @@ public class WorldborderMechanics
 		mainclass = plugin;
 	}
 	
-	public void MakeBorderSmaller(Player p, int amount, long time)
+	public void MakeBorderSmaller()
 	{
-		p.getWorld().getWorldBorder().setSize(amount, time);
+		int amountToMove;
 		
+		if((mainclass.WorldBorderCurrent - mainclass.WordBorderDistance) >= mainclass.WorldBorderMin )
+		{
+			amountToMove = mainclass.WorldBorderCurrent - mainclass.WordBorderDistance;
+		}
+		else
+		{
+			amountToMove = mainclass.WorldBorderMin;
+			mainclass.WorldBorderIsRunning = false;
+		}		
+		
+		mainclass.WorldBorderWorld.getWorldBorder().setSize(amountToMove, mainclass.WorldBorderSpeed);
+		
+		mainclass.WorldBorderCurrent = amountToMove;
+		
+	}
+	
+	public void MakeBorderBigger()
+	{
+		int amountToMove;
+		
+		if((mainclass.WorldBorderCurrent + mainclass.WordBorderDistance) <= mainclass.WorldBorderMax)
+		{
+			amountToMove = mainclass.WorldBorderCurrent + mainclass.WordBorderDistance;
+		}
+		else
+		{
+			amountToMove = mainclass.WorldBorderMax;
+		}
+		
+		mainclass.WorldBorderWorld.getWorldBorder().setSize(amountToMove, 1);
+		
+		mainclass.WorldBorderCurrent = amountToMove;
+	}
+
+	public void BorderReset()
+	{
+		mainclass.WorldBorderWorld.getWorldBorder().setSize(mainclass.WorldBorderMax, 60);
+		mainclass.WorldBorderCurrent = mainclass.WorldBorderMax;
+		mainclass.WorldBorderIsRunning = false;
 	}
 }
